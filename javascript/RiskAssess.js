85f09195ba109f05660cf9a1aee054ca14302372
amisha pai <amishapaib@gmail.com>
2020-11-18 19:11:03 +0530

    Update RiskAssess.js
    fixed a bug

--- javascript/RiskAssess.js
+++ javascript/RiskAssess.js
@@ -3,19 +3,19 @@
 
         
       var pname=document.getElementById("fname").value;
-        var checkdiabetes =document.getElementById(illness1);
-        var checkrespiratory =document.getElementById(illness2);
-        var checkabnormalBP =document.getElementById(illness2);
+        var checkdiabetes = document.getElementById("illness1").checked;
+        var checkrespiratory =document.getElementById("illness2").checked;
+        var checkabnormalBP =document.getElementById("illness2").checked;
         var stdate = new Date(document.getElementById("sdate").value);
         var dedate=new Date(document.getElementById("rdate").value);
         var Difference_In_Time = dedate.getTime() - stdate.getTime(); 
-  
+      
         // To calculate the no. of days between two dates 
         var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
             
         var delayreport=Difference_In_Days;
         var age = document.getElementById("ageNo").value;
-        
+    
         //VAriables to calculate
         var agerf=0;
         var rf=0;
@@ -29,29 +29,19 @@
       
         if (checkdiabetes)
         {
-    
-            diabetes = true ; 
-        }
-        else {
-            diabetes = false ; 
+            rf = rf + drf ;
         }
 
         if (checkrespiratory)
         {
-            Respiratory = true ; 
-        }
-        else {
-            Respiratory = false ; 
+            rf = rf + rrf ;
         }
 
         if (checkabnormalBP)
         {
-            AbnormalBP = true ; 
+            rf = rf + abprf ;
         }
-        else {
-            AbnormalBP = false ; 
-        }
-
+      
         //calculating agerf 
         if (age>=0 && age<=20)
         {
@@ -75,20 +65,10 @@
         }
        
 
-        rf = agerf ; 
-        if (Respiratory)
-        {
-            rf = rf + rrf ;
-        }
-        if (diabetes)
-        {
-            rf = rf + drf ;
-        }
-        if (AbnormalBP)
-        {
-            rf = rf + abprf ;
-        }
-       //calculating agerf 
+        rf = rf + agerf ; 
+
+        
+     //calculating agerf 
        if (delayreport>10)
        {
            rf = rf+1 ; 
@@ -98,7 +78,7 @@
        {
            rf=10;
        }
-    
+      
        document.getElementById("rffor").innerHTML = pname +", your Risk Factor is " + rf
         document.getElementById("results").style.display="inline";
         document.getElementById("myRange").value=rf;
